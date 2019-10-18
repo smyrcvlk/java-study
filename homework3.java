7f5b14eeef395314d3ba6197aa9802fc3f97c3eb
smyrcvlk <smyrcvlk@gmail.com>
2019-10-17 20:56:28 -0400

    Homework 1

---
+++ homework3.java
@@ -0,0 +1,27 @@
+package HOMEWORKS;
+
+public class homework3 {
+    public static void main(String[] args) {
+        //Find the Largest Number Among Three Numbers using user Input
+//to find largest number among three numbers using nested if
+//    int a = 34;
+//    int b = 45;
+//    int c = 78;
+//
+//int num1 = 10, num2 = 20, num3 = 7;
+        //output 20 is the largest Number
+        int num1 = 10;
+        int num2 = 20;
+        int num3 = 7;
+
+        if (num1 > num2 && num1 > num3) {
+            System.out.println(num1 + " is the largest number");
+        } else if (num2 > num1 && num2 > num3) {
+            System.out.println(num2 + " is the largest number");
+        } else {
+            System.out.println(num3 + " is the largest number");
+
+        }
+    }
+
+}
